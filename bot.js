57a042c2f206bb6a372258d203a65898d574b3be
TNThacker2015 <37024464+TNThacker2015@users.noreply.github.com>
2018-10-14 17:37:29 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,23 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client({ disableEveryone: true, autoReconnect: true });
 const JSON = require('circular-json');
+const log = require("better-umi-log");
+const Config = require("./Config.js");
+let applications = JSON.parse(fs.readFileSync("./applications.json", "utf8"));
+bot.on("message", msg => {
+    if (msg.author.id == bot.user.id || msg.author.bot) return;
+  if (!msg.content.startsWith(prefixes[msg.guild.id])) {
+      return;
+    }
+  let channel = msg.channel;
+    let content = msg.content;
+    let text = content.toLowerCase();
+    let args = content.split(" ");
+    let argsLower = text.split(" ");
+    let command = text.substring(Config.prefix.length, argsLower[0].length);
+  
+});
+fs.writeFile("./applications.json", JSON.stringify(applications), (err) => {
+    if (err) console.error(err)
+  });
+bot.login(Config.token);
