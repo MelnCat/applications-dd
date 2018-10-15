9635df75f45b97722b5f91c8bf51ccc4b456f791
TNThacker2015 <37024464+TNThacker2015@users.noreply.github.com>
2018-10-14 18:37:36 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const JSON = require('circular-json');
 const log = require("better-umi-log");
 const Config = require("./Config.js");
 const fs = require("fs");
+bot.user.setPresence({ game: { name: 'for applications. | Prefix: d;' , type: 'WATCHING'} })
 let applications = JSON.parse(fs.readFileSync("./applications.json", "utf8"));
 bot.on("message", msg => {
     if (msg.author.id == bot.user.id || msg.author.bot) return;
@@ -22,6 +23,16 @@ bot.on("message", msg => {
           pingMsg.edit(`:ping_pong: Took \`${Math.round(Date.now() - startTime)}ms\`.`);
         });
   }
+    if (command == "help") {
+        let embed = new Discord.MessageEmbed()
+        embed.setTitle("Help")
+        embed.addField("help", "Opens up the help menu!")
+        embed.addField("ping", "Pings our servers!")
+        embed.addField("apply", "Apply for employee!")
+        embed.setColor(Math.floor(Math.random()*16777216))
+      return channel.send(embed)
+  }
+    //end
 });
 fs.writeFile("./applications.json", JSON.stringify(applications), (err) => {
     if (err) console.error(err)
